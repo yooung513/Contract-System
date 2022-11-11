80769d5b8c215d7659460238cc0937df3e326786
yooung513 <yooung513@gmail.com>
2022-11-11 17:25:59 +0900

    전체 코드 병합 및 충돌, 시큐리티 설정 변경 완료

--- src/main/java/ltoss/dma/DmaApplication.java
+++ src/main/java/ltoss/dma/DmaApplication.java
@@ -1,6 +1,5 @@
 package ltoss.dma;
 
-import ltoss.dma.config.ExchangeRateConfig;
 import ltoss.dma.price.config.PriceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +7,8 @@ import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
-@SpringBootApplication(scanBasePackages = "ltoss.dma.price.web")
-@Import({PriceConfig.class, ExchangeRateConfig.class})
+@SpringBootApplication(scanBasePackages = {"ltoss.dma.price.web", "ltoss.dma.news", "ltoss.dma.login", "ltoss.dma.exchangerate"})
+@Import({PriceConfig.class})
 @EnableJpaAuditing
 public class DmaApplication {
 	public static void main(String[] args) {
