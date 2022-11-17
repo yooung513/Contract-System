21555d284359a9502899ed2f83e87edaa2429ce8
ltoss20221203 <ltoss20221203@gmail.com>
2022-11-17 17:46:47 +0900

    유저 전체 목록 조회 기능 구현

---
+++ src/main/java/ltoss/dma/login/controller/UserController.java
@@ -0,0 +1,23 @@
+package ltoss.dma.login.controller;
+
+import ltoss.dma.login.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllUsers() {
+        return ResponseEntity.ok(userRepository.findAll());
+    }
+}
