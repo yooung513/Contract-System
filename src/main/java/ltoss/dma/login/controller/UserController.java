08b92d1e7d292d5d4baff930d79607268f9e611c
ltoss20221203 <ltoss20221203@gmail.com>
2022-11-17 18:00:33 +0900

    유저 전체 목록 조회 수정

--- src/main/java/ltoss/dma/login/controller/UserController.java
+++ src/main/java/ltoss/dma/login/controller/UserController.java
@@ -1,12 +1,17 @@
 package ltoss.dma.login.controller;
 
+import ltoss.dma.login.models.User;
 import ltoss.dma.login.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
 
     private final UserRepository userRepository;
@@ -18,6 +23,7 @@ public class UserController {
 
     @GetMapping("/all")
     public ResponseEntity<?> getAllUsers() {
-        return ResponseEntity.ok(userRepository.findAll());
+        List<User> user = userRepository.findAll();
+        return ResponseEntity.ok(user);
     }
 }
