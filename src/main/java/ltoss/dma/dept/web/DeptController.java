6331ffb9353a5f4be548860eea1df5c070039338
shampong_ <ghdtjdals1216@gmail.com>
2022-11-25 20:48:56 +0900

    git 테스트

--- src/main/java/ltoss/dma/dept/web/DeptController.java
+++ src/main/java/ltoss/dma/dept/web/DeptController.java
@@ -1,4 +1,13 @@
 package ltoss.dma.dept.web;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@CrossOrigin(origins = "*")
+@RestController
+@AllArgsConstructor
 public class DeptController {
 }
